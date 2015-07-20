01bbee0335a0cc694d15f11070f3c642454bd49d
almarkov <almarkov89@gmail.com>
2015-07-20 17:54:09 +0300

    fixes

--- routes/card_reader.js
+++ routes/card_reader.js
@@ -7,13 +7,15 @@ var http   = require('http');
 //-----------------------------------------------------------------------------
 router.get('/card_ok/:parameter', function(req, res, next) {
 
-	if (devices.get('card_reader').state != "passed") {
-		devices.get('card_reader').state = "passed";
+	if (gamers.quest_state == 170) {
 
-		//  открываем дверь 6
-		helpers.send_get('door_6', 'open', '0', helpers.get_timeout('T1'), ENABLE_MUTEX);
+		if (devices.get('card_reader').state != "passed") {
+			devices.get('card_reader').state = "passed";
 
-		
+			//  открываем дверь 6
+			helpers.send_get('door_6', 'open', '0', helpers.get_timeout('T1'), ENABLE_MUTEX);
+
+		}
 	}
 
 	res.json({success: 1});
