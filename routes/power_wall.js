79fece69737391efa1c10a7173240a30148f9786
almarkov <almarkov89@gmail.com>
2015-07-21 12:15:01 +0300

    powerwall fix

--- routes/power_wall.js
+++ routes/power_wall.js
@@ -6,18 +6,21 @@ var http   = require('http');
 // события
 //-----------------------------------------------------------------------------
 router.get('/power_ok/:code', function(req, res, next) {
-	devices.get('power_wall').state = 'passed';
-	//  открываем дверь 8
-	helpers.send_get('door_8', 'open', '0', helpers.get_timeout('T1'), ENABLE_MUTEX);
 
-	// включаем звук на канале 2 плеера 1 
-	helpers.send_get('audio_player_1', 'play_channel_2', config.audio_files[21].value, DISABLE_TIMER, ENABLE_MUTEX,
-		function(params){
-			var device   = devices.get('audio_player_1');
-			device.value = config.audio_files[21].alias;
-			device.state = "ch1_play_ch2_stop";
-		}, {}
-	);
+	if (gamers.quest_state == 190) {
+		devices.get('power_wall').state = 'passed';
+		//  открываем дверь 8
+		helpers.send_get('door_8', 'open', '0', helpers.get_timeout('T1'), ENABLE_MUTEX);
+
+		// включаем звук на канале 2 плеера 1 
+		helpers.send_get('audio_player_1', 'play_channel_2', config.audio_files[21].value, DISABLE_TIMER, ENABLE_MUTEX,
+			function(params){
+				var device   = devices.get('audio_player_1');
+				device.value = config.audio_files[21].alias;
+				device.state = "ch1_play_ch2_stop";
+			}, {}
+		);
+	}
 
 	res.json({result: 1});
 });
