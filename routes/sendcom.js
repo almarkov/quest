48d0d812d18ebd1bf9364264c9e531f57c08eeb2
almarkov <almarkov89@gmail.com>
2015-07-26 23:06:55 +0300

    sendcom

---
+++ routes/sendcom.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var http   = require('http');
+var router = express.Router();
+var child_process = require('child_process');
+
+// включить arduino 
+router.get('/on/:name', function(req, res, next) {
+	var num;
+	if (req.params.name == 'all') {
+		num = 254;
+	} else {
+		num = devices.get(req.params.name).carrier_id;
+	}
+	child_process.exec('sendcom.exe ' + config.port_num + ' ' + num + ' 1', function(error, stdout, stderr){
+		simple_log('on: ' + req.params.name + ', carrier_id: ' + num);
+	});
+	
+	res.json({success: 1});
+});
+
+// выключить arduino 
+router.get('/off/:name', function(req, res, next) {
+	var num;
+	if (req.params.name == 'all') {
+		num = 254;
+	} else {
+		num = devices.get(req.params.name).carrier_id;
+	}
+	child_process.exec('sendcom.exe ' + num, function(error, stdout, stderr){
+		simple_log('off: ' + req.params.name + ', carrier_id: ' + num);
+	});
+	res.json({success: 1});
+});
+
+module.exports = router;
