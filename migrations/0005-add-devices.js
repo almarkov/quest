8cafd6e37609596906218bf4ebcce79edf2e6a3e
almarkov <almarkov89@gmail.com>
2015-08-03 19:13:28 +0300

    procfile

--- migrations/0005-add-devices.js
+++ migrations/0005-add-devices.js
@@ -2,11 +2,11 @@
 var mongodb = require('mongodb');
 
 exports.up = function(db, next){
-    var device_types = db.collection("device_types");
-    device_types.insert({title: 'Дверь', name: 'door'}, next);
+	var device_types = db.collection("device_types");
+	device_types.insert({title: 'Дверь', name: 'door'}, next);
 };
 
 exports.down = function(db, next){
-    var device_types = db.collection("device_types");
-    device_types.remove({}, {}, next);
+	var device_types = db.collection("device_types");
+	device_types.remove({}, {}, next);
 };
