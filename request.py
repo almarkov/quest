754a40e0be2842168fcd5d2e73faf5926ef3e121
almarkov <almarkov89@gmail.com>
2015-11-24 17:47:38 +0300

    refatoring request.py

--- request.py
+++ request.py
@@ -7,39 +7,36 @@ def out_gpio(value):
 	f_val.write(value)
 	f_val.close()
 
+def send_req(req, write_wait):
+	out_gpio('1')
+	port.write(req[1:])
+	time.sleep(write_wait)
+	out_gpio('0')
+	f = 0
+	while(port.inWaiting() == 0 and f < 5):
+		time.sleep(0.01)
+		f = f+1
+	res = port.read(ord(req[0]))
+	return res
+
+
 def make_request(req):
+	res = ''
 	# watchdog
 	if ord(req[0]) == 255:
-		res = ''
 		for i in range(0,ord(req[1])):
-			out_gpio('1')
-			port.write(req[3+5*i:7+5*i])
-			time.sleep(0.004)
-			out_gpio('0')
-			f = 0
-			while(port.inWaiting() == 0 and f < 5):
-				time.sleep(0.01)
-				f = f+1
-			res += port.read(ord(req[2+5*i]))
-		sys.stdout.write(res)
-
+			res += send_req(req[2+5*i:7+5*i], 0.004)
 	# command
 	else:
-		out_gpio('1')
-		port.write(req[1:])
-		time.sleep(0.004)
-		out_gpio('0')
-		f = 0
-		while(port.inWaiting() == 0 and f < 5):
-			time.sleep(0.01)
-			f = f+1
-		res = port.read(ord(req[0]))
-		sys.stdout.write(res)
+		res = send_req(req, 0.006)
+
 
 
 port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.1)
 
 req = sys.stdin.read()
 
-make_request(req)
+res = make_request(req)
+
+sys.stdout.write(res)
 
