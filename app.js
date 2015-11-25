5622d0b47940d971e89cbe9738333aa39d9ba7e6
almarkov <almarkov89@gmail.com>
2015-11-26 01:40:34 +0300

    fix

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger       = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser   = require('body-parser')
 var PythonShell  = require('python-shell')
+var child_process = require('child_process')
 
 
 // вспомогат. ф-ции
