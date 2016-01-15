c5398da29394707a8495a6e73eaa821277f20513
almarkov <almarkov89@gmail.com>
2016-01-16 02:06:20 +0300

    log

--- routes/watchdog.js
+++ routes/watchdog.js
@@ -8,20 +8,20 @@ router.get('/', function(req, res, next) {
 	var device_ids = req.query.device_id || req.query.di
 	var status_ids = req.query.status_id || req.query.si
 	var carrier_id = req.query.carrier_id
-
+mlog.dev('watchdog_')
 	if (globals.get('enable_watchdog')) {
 
 		for(var i = 0; i < device_ids.length; i++) {
 
 			var device = devices.get_by_id(carrier_id, device_ids[i])
-
+			mlog.dev(device.name)
 			if (device && device.wd_enabled) {
 
 				var old_state = device.state;
 
 				//var state = routines.get_by_field(device.states, 'code', status_ids[i])
 				var state = device.states_code_hash[status_ids[i]]
-
+				mlog.dev(state.name)
 				device.state = state.name
 				device.prev_state = old_state;
 
